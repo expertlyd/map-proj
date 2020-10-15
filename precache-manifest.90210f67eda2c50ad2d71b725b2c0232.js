f557c1d4cab6a0296d4e01bd08c0c219fbed331d
Habibullah Oladepo <holadepo@gmail.com>
2020-10-15 06:11:46 +0100

    Updates

---
+++ precache-manifest.90210f67eda2c50ad2d71b725b2c0232.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bb4bbf006d505354bcde4930e448a24",
+    "url": "/map-proj/index.html"
+  },
+  {
+    "revision": "c034074dd3ec4340bf93",
+    "url": "/map-proj/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "5c31de88aa932912f34c",
+    "url": "/map-proj/static/css/main.bcb899e8.chunk.css"
+  },
+  {
+    "revision": "c034074dd3ec4340bf93",
+    "url": "/map-proj/static/js/2.6d3e1eea.chunk.js"
+  },
+  {
+    "revision": "0580f66cff7a7b2e7b5c118fd842bcfd",
+    "url": "/map-proj/static/js/2.6d3e1eea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c31de88aa932912f34c",
+    "url": "/map-proj/static/js/main.d55c27e6.chunk.js"
+  },
+  {
+    "revision": "9fad54e308cbbeec0c82",
+    "url": "/map-proj/static/js/runtime-main.04adcefe.js"
+  },
+  {
+    "revision": "2ceaf661f7c099123415f94f97e2a0c5",
+    "url": "/map-proj/static/media/disaster_location_icon.2ceaf661.svg"
+  }
+]);
